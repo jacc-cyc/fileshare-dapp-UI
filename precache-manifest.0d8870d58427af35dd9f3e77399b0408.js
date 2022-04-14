e153e64ab5ec1536b880a7f0e69f23df8d77a084
Jacky <jackychan121017@gmail.com>
2022-04-14 16:26:21 +0800

    Updates

---
+++ precache-manifest.0d8870d58427af35dd9f3e77399b0408.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4129df9e8a8bdb9522887d9bf7eca443",
+    "url": "/fileshare-dapp-UI/index.html"
+  },
+  {
+    "revision": "179d58b06f7b542ea33b",
+    "url": "/fileshare-dapp-UI/static/css/main.9d0d214d.chunk.css"
+  },
+  {
+    "revision": "9e6ea65474e2d1d9bbcd",
+    "url": "/fileshare-dapp-UI/static/js/2.dc53fe23.chunk.js"
+  },
+  {
+    "revision": "dba0c006430f86ab5dfd81528eb42c84",
+    "url": "/fileshare-dapp-UI/static/js/2.dc53fe23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "179d58b06f7b542ea33b",
+    "url": "/fileshare-dapp-UI/static/js/main.1eabf010.chunk.js"
+  },
+  {
+    "revision": "10f66074301c8efeca61",
+    "url": "/fileshare-dapp-UI/static/js/runtime-main.63358cde.js"
+  }
+]);
